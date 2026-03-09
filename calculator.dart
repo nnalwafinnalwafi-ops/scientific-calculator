2cfb9e92dae8d036fcba7b70badec6bf30085f93
nnalwafinnalwafi-ops <nnalwafinnalwafi@gmail.com>
2026-03-10 00:37:10 +0300

    Add basic arithmetic and trigonometric functions

---
+++ calculator.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+double add(double a, double b) => a + b;
+double subtract(double a, double b) => a - b;
+double multiply(double a, double b) => a * b;
+double divide(double a, double b) => a / b;
+
+double sinValue(double x) => sin(x);
+double cosValue(double x) => cos(x);
+double tanValue(double x) => tan(x);
+double squareRoot(double x) => sqrt(x);
